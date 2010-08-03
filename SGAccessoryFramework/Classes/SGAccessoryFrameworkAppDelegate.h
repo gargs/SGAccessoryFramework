c6db45a235e7dd1c4d9b7597409db21071799281
Saurabh Garg <saurabh@gargs.com>
2010-08-03 22:59:38 +0530

    Skeleton for a new application with the External Accessory framework added.

---
+++ SGAccessoryFramework/Classes/SGAccessoryFrameworkAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SGAccessoryFrameworkAppDelegate.h
+//  SGAccessoryFramework
+//
+//  Created by Saurabh Garg on 8/3/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAccessoryFrameworkAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
